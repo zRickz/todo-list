00c91379befdb2d633dc80514f1ebbd237f14740
Rick <59352875+zRickz@users.noreply.github.com>
2023-08-18 18:04:11 -0300

    Add files via upload

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,6 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
+app_1.default.use((err, req, res, next) => {
+    if (err.code === 'EBADCSRFTOKEN') {
+        return res.status(403).send({ message: 'CSRF Inválido' });
+    }
+    return next(err);
+});
 app_1.default.listen(8080, () => {
     console.log('Servidor ligado em http://localhost:8080');
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,12 @@
 import app from './app'
+import { Request, Response, NextFunction } from 'express'
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err.code === 'EBADCSRFTOKEN') {
+      return res.status(403).send({ message: 'CSRF Inválido' });
+    }
+    return next(err);
+  });
 
 app.listen(8080, () => {
     console.log('Servidor ligado em http://localhost:8080')
